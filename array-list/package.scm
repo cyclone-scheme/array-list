87c237be7c775bf514a288039e328af8dcf708ff
Justin Ethier <justin.ethier@gmail.com>
2019-07-18 18:09:23 -0400

    Adding for cyclone-winds

---
+++ array-list/package.scm
@@ -0,0 +1,13 @@
+(package
+ (name           array-list)
+ (version        0.1)
+ (license        "MIT")
+ (authors        "Koz Ross")
+ (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
+ (description    "Array list data structure")
+ (tags           "math" "data-structures")
+ (docs           "https://github.com/cyclone-scheme/cyclone-winds/wiki/array-list.md")
+
+ (library
+   (name (cyclone array-list))
+   (description "array-list library")))
