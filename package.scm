bb34b516210a71cbed6aeb316ab342fd90df1849
Justin Ethier <justin.ethier@gmail.com>
2021-06-28 18:28:46 -0700

    Use semantic version

--- package.scm
+++ package.scm
@@ -1,6 +1,6 @@
 (package
  (name           array-list)
- (version        0.1)
+ (version        "0.1.1")
  (license        "MIT")
  (authors        "Koz Ross")
  (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
